f227e29f11478bc3b0eca97e811e2b30fa0a1c55
Etienne Bossuet <e.bossuet@gmail.com>
2013-01-08 11:30:56 +0100

    Ajout d'un package test

---
+++ echec/src/test/Test.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Bob
+ *
+ */
+public class Test {
+
+	@org.junit.Test
+	public void test() {
+		int n = 8;
+		assertEquals(n,8);
+	}
+}
