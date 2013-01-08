9e8c148530e34a2dd74f87cb746c6adac4e33131
Florent Souillard <Florent@MacBook-Pro-de-Florent.local>
2013-01-08 13:46:02 +0100

    Ajout initialisation grille

--- echec/src/model/Grille.java
+++ echec/src/model/Grille.java
@@ -15,6 +15,33 @@ public class Grille {
 	}
 	
 	public void initialisation(){
+		
+		for(int i = 0; i < m_largeur; i++)
+		{
+			m_grille[i][1] = new Pion(i, 1, "Noir");
+			m_grille[i][6] = new Pion(i, 1, "Blanc");
+		}
+		
+		m_grille[0][0] = new Tour(0, 0, "Noir");
+		m_grille[0][m_largeur-1] = new Tour(0, m_largeur-1, "Noir");
+		m_grille[0][1] = new Cavalier(0, 1, "Noir");
+		m_grille[0][m_largeur-2] = new Cavalier(0, m_largeur-2, "Noir");
+		m_grille[0][2] = new Fou(0, 2, "Noir");
+		m_grille[0][m_largeur-3] = new Fou(0, m_largeur-3, "Noir");
+		m_grille[0][3] = new Dame(0, 3, "Noir");
+		m_grille[0][4] = new Roi(0, 4, "Noir");
+		
+		m_grille[6][0] = new Tour(6, 0, "Noir");
+		m_grille[6][m_largeur-1] = new Tour(6, m_largeur-1, "Noir");
+		m_grille[6][1] = new Cavalier(6, 1, "Noir");
+		m_grille[6][m_largeur-2] = new Cavalier(6, m_largeur-2, "Noir");
+		m_grille[6][2] = new Fou(6, 2, "Noir");
+		m_grille[6][m_largeur-3] = new Fou(6, m_largeur-3, "Noir");
+		m_grille[6][3] = new Dame(6, 3, "Noir");
+		m_grille[6][4] = new Roi(6, 4, "Noir");
+		
+		
 	}
+	
 
 }
