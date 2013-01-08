a9e83e2873a9c5cfb7fb211ea5a5b701426cdb57
Florent Souillard <Florent@MacBook-Pro-de-Florent.local>
2013-01-08 12:25:10 +0100

    modif classe Piece

--- echec/src/model/Piece.java
+++ echec/src/model/Piece.java
@@ -1,5 +1,19 @@
 package model;
 
-public class Piece {
-
+public abstract class Piece {
+	
+	private int m_x;
+	private int m_y;
+	private String m_color;
+	private boolean is_deleted;
+	
+	
+	public Piece(int x, int y, String color)
+	{
+		m_x = x;
+		m_y = y;
+		m_color = color;
+		is_deleted = false;
+	}
+	
 }
